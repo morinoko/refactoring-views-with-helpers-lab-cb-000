a9bb447427cc7db34284af0dc2a711afe333bd92
Felice Forby <feliceforby@gmail.com>
2018-10-25 23:23:23 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,7 +6,9 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
-    artist = Artist.find_or_create_by(name: name)
-    self.artist = artist
+    if name
+      artist = Artist.find_or_create_by(name: name)
+      self.artist = artist
+    end
   end
 end
