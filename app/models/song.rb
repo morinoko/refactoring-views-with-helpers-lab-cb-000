0a9b229ba4a618e3b1a571f86f868569fcf10ef6
Felice Forby <feliceforby@gmail.com>
2018-10-25 23:30:22 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,15 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist.name if self.artist
   end
 
   def artist_name=(name)
+    if name.blank?
+      self.artist = nil
+    else
+      artist = Artist.find_or_create_by(name: name)
+      self.artist = artist
+    end
   end
 end
