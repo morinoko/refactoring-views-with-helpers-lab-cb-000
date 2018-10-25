4bbb97132709c000f572ccc9dc37b90b9221c49a
Felice Forby <feliceforby@gmail.com>
2018-10-25 23:04:29 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -3,7 +3,7 @@ module SongsHelper
     if song.artist
       link_to song.artist_name, artist_path(song.artist)
     else
-      link_to "Add artist", song_edit_path(song)
+      link_to "Add artist", edit_song_path(song)
     end
   end
 end
